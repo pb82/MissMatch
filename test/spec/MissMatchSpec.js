7ae20bfa34730cc790a478b8d39d6e05e18100bf
Peter Braun <peter-braun@gmx.net>
2012-07-09 22:55:44 +0200

    API cleanup & some tests

--- test/spec/MissMatchSpec.js
+++ test/spec/MissMatchSpec.js
@@ -1,7 +1,22 @@
 describe("Basic tests (module integrity)", function() {
   it("should exist and be available", function() {
-    expect(typeof mm).toBe('function');
+    expect(typeof mm.match).toBe('function');
     expect(typeof mm.json).toBe('function');
     expect(typeof mm.compile).toBe('function');    
+  });  
+});
+
+describe("Simple pattern tests", function() {
+  it("should recognize basic types", function() {
+        
+    var numeric = mm.match(1, { 'n@n': "return this.n" });      
+    var string = mm.match('s', { 's@n': "return this.n" });      
+    var boolean = mm.match(true, { 'b@n': "return this.n" });          
+    var fun = mm.match(function (x) {return x;}, { 'f@n': "return this.n" });
+    
+    expect(numeric).toBe(1);
+    expect(string).toBe('s');
+    expect(boolean).toBe(true);
+    expect(fun(1)).toBe(1);
   });  
 });
