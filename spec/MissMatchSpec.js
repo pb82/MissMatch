0862f7bbe65bebc576f24f75d7383489e41c5aa0
Peter Braun <peter-braun@gmx.net>
2012-07-09 22:22:42 +0200

    removed old files

--- spec/MissMatchSpec.js
+++
@@ -1,7 +0,0 @@
-describe("Basic tests (module integrity)", function() {
-  it("should exist and be available", function() {
-    expect(typeof mm).toBe('function');
-    expect(typeof mm.json).toBe('function');
-    expect(typeof mm.compile).toBe('function');    
-  });  
-});
